e59ea9ffb28e16b1dce1d85c190fbdd335e4d0e3
Shaban  Harouna <kmharouna@gmail.com>
2022-04-13 15:47:00 +0300

    added schema/pydantic model for post responses

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from typing import Optional
+
+from pydantic.types import conint
 
 
 class PostBase(BaseModel):
@@ -9,3 +13,22 @@ class PostBase(BaseModel):
 
 class PostCreate(PostBase):
     pass
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
