7e8eb9915cee9abd9a573371528df024dccdf01c
Shaban  Harouna <kmharouna@gmail.com>
2022-04-13 11:07:25 +0300

    setting up postgres adapter and sqlachemy orm in project

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from fastapi.params import Body
+# from pydantic import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+
+try:
+    conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',password='charmz007', cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    print("Database connection was successfull!")
+except Exception as error:
+    print("Connecting to database failed")
+    print("Error: ", error)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to our api"}
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"Status": "success"}
+
+
+
