259941a77eacd25cd2826d17374920c10c0fae3b
Shaban  Harouna <kmharouna@gmail.com>
2022-04-25 11:54:36 +0300

    implemented search queries & cleaned up the main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from pydantic import BaseModel
-
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
 
 
@@ -15,15 +8,6 @@ from .routers import post, user, auth
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-try:
-    conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',password='charmz007', cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("Database connection was successfull!")
-except Exception as error:
-    print("Connecting to database failed")
-    print("Error: ", error)
 
 app.include_router(post.router)
 app.include_router(user.router)
