da095cba41b4675310c678ebe110d00df6618108
Shaban  Harouna <kmharouna@gmail.com>
2022-04-26 14:27:17 +0300

    implemented sql alchemy join for get_posts end point

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List, Optional
 from .. import models, schemas, oauth2
 from ..database import get_db
@@ -9,8 +10,8 @@ router = APIRouter(
     prefix="/posts",
     tags=['Posts']
 )
-
-@router.get('/', response_model=List[schemas.Post])
+# @router.get('/', response_model=List[schemas.Post])
+@router.get('/', response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),limit:int = 10,skip:int=0,search:Optional[str]=""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
@@ -18,7 +19,10 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # in the event when we want to retrieve only posts created by the logged-in user
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
 
-    return posts
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    
+
+    return results
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
